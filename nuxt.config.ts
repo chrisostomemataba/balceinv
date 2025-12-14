875e36c6101c9060249c2b260c108c933b340351
chrisostomemataba <doctorchrisostome@gmail.com>
2025-12-14 10:11:17 +0300

    feat(card): implement Card component with subcomponents for structured UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
+
   vite: {
     plugins: [
       tailwindcss() as any,
     ],
   },
+
+  modules: ["shadcn-nuxt"],
 });
